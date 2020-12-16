63cf8e4659f7197d47d10f46a2c6cfb42eb57cb7
niza <niza@cego.dk>
2020-12-16 13:07:08 +0100

    Added app and ran fixer

--- src/SeamlessWallet/SeamlessWallet.php
+++ src/SeamlessWallet/SeamlessWallet.php
@@ -3,8 +3,8 @@
 namespace Cego\SeamlessWallet;
 
 use InvalidArgumentException;
-use Illuminate\Support\Facades\Http;
 use Illuminate\Http\Client\Response;
+use Illuminate\Support\Facades\Http;
 use Cego\SeamlessWallet\Exceptions\SeamlessWalletRequestFailedException;
 
 /**
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -8,9 +8,7 @@ use PHPUnit\Framework\TestCase as PHPUnitTestCase;
  * Class TestCase
  *
  * Used for implementing common method across test cases
- *
  */
 class TestCase extends PHPUnitTestCase
 {
-
 }
