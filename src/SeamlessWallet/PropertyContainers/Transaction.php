e7fc1e967fa771e72fc7f60c7f67ce168faf01c6
niza <niza@cego.dk>
2021-03-01 09:19:45 +0100

    Added external name

--- src/SeamlessWallet/PropertyContainers/Transaction.php
+++ src/SeamlessWallet/PropertyContainers/Transaction.php
@@ -10,6 +10,7 @@ use Carbon\Carbon;
  * @property-read int $transaction_type_id
  * @property-read int $transaction_context_id
  * @property-read string $amount
+ * @property-read ?string $externalName
  * @property-read Carbon|null $rolled_back_at
  * @property-read Carbon|null $created_at
  * @property-read Carbon|null $updated_at
